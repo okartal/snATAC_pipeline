e3efddc7a54352ee590e6d7ed62322796267567b
frank@tscc <zhangc518@gmail.com>
2017-10-28 02:46:54 -0700

    fixed more bugs; now it can run through

--- bin/dedup_cell.sh
+++ bin/dedup_cell.sh
@@ -2,7 +2,7 @@
 PREFIX=$1; THREADS=$2; 
 
 iterator=0
-ls $prefix\_tmp/cells | grep .sam | sed "s/\.sam//g" | xargs -i --max-procs=$THREADS \
+ls $PREFIX\_tmp/cells | grep .sam | sed "s/\.sam//g" | xargs -i --max-procs=$THREADS \
                                           bash -c "samtools view -bS $PREFIX\_tmp/cells/{}.sam \
 	    | samtools sort - -o $PREFIX\_tmp/cells/{}.sorted.bam  >/dev/null 2>&1; \
 samtools rmdup $PREFIX\_tmp/cells/{}.sorted.bam $PREFIX\_tmp/cells/{}.sorted.nodup.bam >/dev/null 2>&1"
