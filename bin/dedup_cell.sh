a6305c03217e1c5f5c4a70efe526caf653a554f7
frank@tscc <zhangc518@gmail.com>
2017-10-28 02:02:51 -0700

    use 1M reads

--- bin/dedup_cell.sh
+++ bin/dedup_cell.sh
@@ -2,20 +2,9 @@
 PREFIX=$1; THREADS=$2; 
 
 iterator=0
-for line in `ls $PREFIX\_tmp/cells | grep .sam`
-do
-	barcode="${line%.*}"
-	samtools view -bS $PREFIX\_tmp/cells/$barcode.sam \
-	    | samtools sort -@ $THREADS - -o $PREFIX\_tmp/cells/$barcode.sorted.bam  >/dev/null 2>&1
-                                                                               
-	samtools rmdup $PREFIX\_tmp/cells/$barcode.sorted.bam \
-                 $PREFIX\_tmp/cells/$barcode.sorted.nodup.bam >/dev/null 2>&1
-    iterator=$((iterator + 1))
-    if (( $iterator % $THREADS == 0 ))           # no need for brackets
-    then
-  	  while kill -0 $! 2>/dev/null
-  	  do
-  	    sleep 5
-  	  done
-    fi
-done
+ls $prefix\_tmp/cells | grep .sam | sed "s/\.sam//g" | xargs -i --max-procs=$THREADS \
+                                          bash -c "samtools view -bS $PREFIX\_tmp/cells/{}.sam \
+	    | samtools sort - -o $PREFIX\_tmp/cells/{}.sorted.bam  >/dev/null 2>&1; \
+samtools rmdup $PREFIX\_tmp/cells/{}.sorted.bam $PREFIX\_tmp/cells/{}.sorted.nodup.bam >/dev/null 2>&1"
+
+
